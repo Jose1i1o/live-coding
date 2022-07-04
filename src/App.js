7f1d9aea4bb4792ec0363decfffce7e1e644b807
Jose1i1o <guadix88@gmail.com>
2022-07-04 16:23:53 +0200

    Live-coding session 04-Jul

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Products from './components/Products';
+// import Article from './components/Article';
+
+
+function App(props) {
+
+  return (
+      // <Article {...props}/>
+    <>
+      <Products  />
+    </>
+  );
+}
+
+export default App;
