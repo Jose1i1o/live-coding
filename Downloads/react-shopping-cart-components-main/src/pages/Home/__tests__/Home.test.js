e56341b0be20a0d0eff0d7d7952ab9302e755c3d
José Valenzuela <josevalenzuela@MacBook-Air-de-Jose.local>
2022-07-18 18:23:26 +0200

    fixed folder

--- Downloads/react-shopping-cart-components-main/src/pages/Home/__tests__/Home.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Home from "..";
-
-describe("<Home />", () => {
-  it("is defined", () => {
-    expect(<Home />).toBeDefined();
-  });
-});
--- README.md
+++
@@ -1,1 +0,0 @@
-# bojji_cv
