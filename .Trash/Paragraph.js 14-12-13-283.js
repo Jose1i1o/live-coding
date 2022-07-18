fd54223ddc573b64da5eef850be4ac22fdc7c8e6
José Valenzuela <josevalenzuela@MacBook-Air-de-Jose.local>
2022-07-18 18:27:58 +0200

    fixed errors

--- .Trash/Paragraph.js 14-12-13-283.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Paragraph = (props) => {
-  return (
-    <div>{props.paragraph}</div>
-  )
-}
-
-export default Paragraph
--- .Trash/newProject/src/index.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-import moment from 'moment';
-
-const now = moment().calendar();
-console.log(now);
